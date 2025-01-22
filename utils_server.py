31cef727d2011bcc7ce7f1c4853b55201c75051b
SirJonasM <jonas.meowes02@gmail.com>
2025-01-22 16:01:16 +0100

    test

--- utils_server.py
+++ utils_server.py
@@ -8,7 +8,6 @@ from libs_server import SIGNATURE_ALGORITHMS, KEM_ALGORITHMS
 import csv
 from dotenv import load_dotenv
 
-load_dotenv()
 
 
 def hash_message(message, timings):
@@ -87,6 +86,7 @@ def decapsulate_aes_key(secret_key_encrypted, kem_algo_name, timings):
 def write_timings_to_file(
     timings, kem_algo_name, sign_algorithm_name, output_file="server_timings.csv"
 ):
+    load_dotenv()
     client_device = os.getenv("DEVICE_NAME")
     # Prepare the data row with algorithm names and timings
     data_row = [kem_algo_name, sign_algorithm_name, client_device] + list(timings.values())
